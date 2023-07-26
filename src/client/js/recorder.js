0f49624dcaff68e3eb94bb8b7546f6fe2c423910
hotaffogato <kshmine6@gmail.com>
2023-07-27 03:37:17 +0900

    edit stream

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,23 +1,58 @@
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
 const startBtn = document.getElementById("startBtn")
 const video = document.getElementById("preview")
 
-const handleStart = async() => {
+let recorder;
+let stream;
+let videoFile;
+
+const handleDownload = async () => {
+  const ffmpeg = createFFmpeg({ log: true });
+  await ffmpeg.load();
+  ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
+  await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
+  const mp4File = ffmpeg.FS("readFile", "output.mp4");
+  const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
+  const mp4Url = URL.createObjectURL(mp4Blob);
+  const a = document.createElement("a");
+  a.href = mp4Url;
+  a.download = "MyRecording.mp4";
+  document.body.appendChild(a);
+  a.click();
+}
+
+const handleStop = () => {
+    startBtn.innerText = "Start Recording"
+    startBtn.removeEventListener("click", handleStop)
+    startBtn.addEventListener("click", handleDownload)
+    recorder.stop()
+}
+
+const handleStart = () => {
+    startBtn.innerText = "Stop Recording"
+    startBtn.removeEventListener("click", handleStart)
+    startBtn.addEventListener("click", handleStop)
+    recorder = new MediaRecorder(stream,{mimeType:"video/webm"});
+    recorder.ondataavailable = (event) => {
+        videoFile = URL.createObjectURL(event.data);
+        video.srcObject = null;
+        video.src = videoFile;
+        video.loop = true;
+        video.play();
+    }
+    recorder.start();
+}
+
+const init = async() => {
+    //캠 스트리밍 시작,  
     stream = await navigator.mediaDevices.getUserMedia({
-        audio:true,
+        audio:false,
         video:true
     });
-    console.log(stream)
-
     video.srcObject = stream;
     video.play();
-
-    try {
-
-        /* use the stream */
-    } catch (err) {
-
-        /* handle the error */
-    }
 }
+
+init();
 
 startBtn.addEventListener("click", handleStart)
